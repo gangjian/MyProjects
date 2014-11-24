c28828ba76a1c0a8de7dff7400493d34ff7ef55b
gangjian <gangjian.mail@qq.com>
2014-11-24 19:17:00 +0900

    2014/11/24 SuperKey试验

---
+++ SuperKeys/DEMO/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DEMO
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.labelSecond = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(106, 108);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 1;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // labelSecond
+            // 
+            this.labelSecond.AutoSize = true;
+            this.labelSecond.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelSecond.Location = new System.Drawing.Point(124, 9);
+            this.labelSecond.Name = "labelSecond";
+            this.labelSecond.Size = new System.Drawing.Size(0, 27);
+            this.labelSecond.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelSecond);
+            this.groupBox1.Location = new System.Drawing.Point(14, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 39);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 44);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "点击Run后，请用鼠标激活“拳皇”游戏窗口，会有3秒的缓冲时间执行“八神”的一个特效。有兴趣的可以使用全局钩子来做。我比较懒*_*!";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 143);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonRun);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "SuperKeys Demo";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Label labelSecond;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ SuperKeys/DEMO/MainForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+using SuperKeys;
+
+namespace DEMO
+{
+    public partial class MainForm : Form
+    {
+        private int m_Second = 4;
+        WinIoSys m_IoSys = new WinIoSys();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            m_Second--;
+            labelSecond.Text = m_Second.ToString();
+            if (m_Second == 0)
+            {
+                //m_IoSys.KeyDown(WinIoSys.Key.VK_S);
+                //Thread.Sleep(100);
+                //m_IoSys.KeyDown(WinIoSys.Key.VK_D);
+                //Thread.Sleep(100);
+                //m_IoSys.KeyUp(WinIoSys.Key.VK_S);
+                //Thread.Sleep(100);
+                //m_IoSys.KeyUp(WinIoSys.Key.VK_D);
+                //Thread.Sleep(10);
+                //m_IoSys.KeyPress(WinIoSys.Key.VK_J, 200);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    m_IoSys.KeyPress(WinIoSys.Key.VK_CONTROL, 100);
+                    m_IoSys.KeyPress(WinIoSys.Key.VK_SHIFT, 100);
+                    //m_IoSys.KeyDown(WinIoSys.Key.VK_SHIFT);
+                    //Thread.Sleep(10);
+                    //m_IoSys.KeyUp(WinIoSys.Key.VK_SHIFT);
+                }
+                
+                timer1.Stop();
+                m_Second = 4;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            m_IoSys.InitSuperKeys();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            m_IoSys.CloseSuperKeys();
+        }
+    }
+}
