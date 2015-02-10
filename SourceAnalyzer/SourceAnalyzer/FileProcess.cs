7a6120a88ae24623e1cbb66d18ff4cd860c9ec0c
gangjian <gangjian.mail@qq.com>
2015-02-10 18:17:40 +0900

    2015/02/10

---
+++ SourceAnalyzer/SourceAnalyzer/FileProcess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SourceAnalyzer
+{
+    class FileProcess
+    {
+        public FileProcess(string fileName)
+        {
+        }
+    }
+}
