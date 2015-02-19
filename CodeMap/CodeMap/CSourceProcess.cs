99b921c305333e565c435f0cea283df535dc74ca
gangjian <gangjian.mail@qq.com>
2015-02-19 17:48:24 +0900

    2015/02/19 大年初一 GetNextIdentifier功能基本完成

--- CodeMap/CodeMap/CSourceProcess.cs
+++ CodeMap/CodeMap/CSourceProcess.cs
@@ -16,6 +16,17 @@ namespace CodeMap
         public bool pop_up_flag = false;
     }
 
+    enum E_CHAR_TYPE
+    {
+        E_CTYPE_WHITE_SPACE,
+        E_CTYPE_LETTER,
+        E_CTYPE_NUMBER,
+        E_CTYPE_UNDERLINE,
+        E_CTYPE_PUNCTUATION,
+        E_CTYPE_SYMBOL,
+        E_CTYPE_UNKNOWN
+    }
+
     class CSourceProcess
     {
         /// <summary>
@@ -26,12 +37,16 @@ namespace CodeMap
         {
             List<string> wtList = RemoveComments(fileName);
             wtList = RemoveConditionalCompile(wtList);
-            TextWriter tw = new StreamWriter(fileName);
-            foreach (string wtLine in wtList)
-            {
-                tw.WriteLine(wtLine);
-            }
-            tw.Close();
+
+            int lineIdx = 0, startIdx = 0;
+            GetCodeBlock(wtList, ref lineIdx, ref startIdx);
+
+            //TextWriter tw = new StreamWriter(fileName);
+            //foreach (string wtLine in wtList)
+            //{
+            //    tw.WriteLine(wtLine);
+            //}
+            //tw.Close();
         }
 
         /// <summary>
@@ -42,12 +57,13 @@ namespace CodeMap
         {
             List<string> wtList = RemoveComments(fileName);
             wtList = RemoveConditionalCompile(wtList);
-            TextWriter tw = new StreamWriter(fileName);
-            foreach (string wtLine in wtList)
-            {
-                tw.WriteLine(wtLine);
-            }
-            tw.Close();
+
+            //TextWriter tw = new StreamWriter(fileName);
+            //foreach (string wtLine in wtList)
+            //{
+            //    tw.WriteLine(wtLine);
+            //}
+            //tw.Close();
         }
 
         /// <summary>
@@ -225,6 +241,190 @@ namespace CodeMap
             }
 
             return retList;
+        }
+
+        /// <summary>
+        /// 取得一块分类代码区块
+        /// </summary>
+        public static void GetCodeBlock(List<string> codeList, ref int lineIdx, ref int startIdx)
+        {
+            System.Diagnostics.Trace.Assert((null != codeList));
+            if (0 == codeList.Count)
+            {
+                return;
+            }
+
+            string nextId = null;
+            while (null != (nextId = GetNextIdentifier(codeList, ref lineIdx, ref startIdx)))
+            {
+                System.Diagnostics.Trace.WriteLine(nextId);
+                if ("rcvcm_conv" == nextId)
+                {
+                    System.Diagnostics.Trace.WriteLine("shit");
+                }
+            }
+
+            return;
+        }
+
+        /// <summary>
+        /// 从当前位置处取得一个标识符(identifier)
+        /// </summary>
+        /// <param name="codeList"></param>
+        /// <param name="lineIdx"></param>
+        /// <param name="startIdx"></param>
+        /// <returns></returns>
+        static string GetNextIdentifier(List<string> codeList, ref int lineIdx, ref int startIdx)
+        {
+            System.Diagnostics.Trace.Assert(lineIdx >= 0);
+            if (lineIdx >= codeList.Count)
+            {
+                return null;
+            }
+
+            string curLine = "";
+            int curIdx = startIdx;
+            int s_pos = -1, e_pos = -1;     // 标识符的起止位置
+            while (true)
+            {
+                // 到list末尾结束跳出
+                if (lineIdx >= codeList.Count)
+                {
+                    break;
+                }
+                curLine = codeList[lineIdx];
+                // 如果当前行是空行, 跳到下一行
+                if (string.Empty == curLine)
+                {
+                    lineIdx++;
+                    continue;
+                }
+
+                // 从这里开始逐字符进行判断
+                bool startFlag = false;
+                for (; curIdx < curLine.Length; curIdx++)
+                {
+                    char curChar = curLine[curIdx];
+                    E_CHAR_TYPE cType = GetCharType(curChar);
+                    switch (cType)
+                    {
+                        case E_CHAR_TYPE.E_CTYPE_WHITE_SPACE:
+                            if (false == startFlag)
+                            {
+                            }
+                            else
+                            {
+                                // 标识符结束
+                                goto RET_IDF;
+                            }
+                            break;
+                        case E_CHAR_TYPE.E_CTYPE_LETTER:
+                        case E_CHAR_TYPE.E_CTYPE_UNDERLINE:
+                            if (false == startFlag)
+                            {
+                                // 标识符开始
+                                startFlag = true;
+                                s_pos = curIdx;
+                                e_pos = curIdx;
+                            }
+                            else
+                            {
+                                // 标识符继续
+                                e_pos = curIdx;
+                            }
+                            break;
+                        case E_CHAR_TYPE.E_CTYPE_NUMBER:
+                            if (false == startFlag)
+                            {
+                                // 标识符结束, 返回该数字
+                                s_pos = curIdx;
+                                e_pos = curIdx;
+                                curIdx++;
+                                goto RET_IDF;
+                            }
+                            else
+                            {
+                                // 标识符继续
+                                e_pos = curIdx;
+                            }
+                            break;
+                        case E_CHAR_TYPE.E_CTYPE_PUNCTUATION:
+                        case E_CHAR_TYPE.E_CTYPE_SYMBOL:
+                            if (false == startFlag)
+                            {
+                                // 标识符结束, 返回该符号
+                                s_pos = curIdx;
+                                e_pos = curIdx;
+                                curIdx++;
+                                goto RET_IDF;
+                            }
+                            else
+                            {
+                                // 标识符结束
+                                goto RET_IDF;
+                            }
+                        case E_CHAR_TYPE.E_CTYPE_UNKNOWN:
+                            return null;
+                    }
+                }
+
+                // 到达行末
+                // 转到下一行开头
+                lineIdx++;
+                curIdx = 0;
+                if (startFlag)
+                {
+                    goto RET_IDF;
+                }
+            }
+            return null;
+RET_IDF:
+            if (-1 != s_pos && -1 != e_pos)
+            {
+                if (curIdx >= curLine.Length)
+                {
+                    lineIdx++;
+                    curIdx = 0;
+                }
+                startIdx = curIdx;
+                return curLine.Substring(s_pos, e_pos - s_pos + 1);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        static E_CHAR_TYPE GetCharType(Char ch)
+        {
+            if (Char.IsWhiteSpace(ch))
+            {
+                return E_CHAR_TYPE.E_CTYPE_WHITE_SPACE;
+            }
+            else if (Char.IsDigit(ch))
+            {
+                return E_CHAR_TYPE.E_CTYPE_NUMBER;
+            }
+            else if (Char.IsLetter(ch))
+            {
+                return E_CHAR_TYPE.E_CTYPE_LETTER;
+            }
+            else if (ch.Equals('_'))
+            {
+                return E_CHAR_TYPE.E_CTYPE_UNDERLINE;
+            }
+            else if (Char.IsPunctuation(ch))
+            {
+                return E_CHAR_TYPE.E_CTYPE_PUNCTUATION;
+            }
+            else if (Char.IsSymbol(ch))
+            {
+                return E_CHAR_TYPE.E_CTYPE_SYMBOL;
+            }
+            else
+            {
+                return E_CHAR_TYPE.E_CTYPE_UNKNOWN;
+            }
         }
     }
 }
