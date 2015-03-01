a479a96438d7805d8c923d4038fc12b9ddea13e4
gangjian <gangjian.mail@qq.com>
2015-03-01 11:59:37 +0900

    2015/03/01

---
+++ CodeMap/CodeMap/XmlProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace CodeMap
+{
+    class XmlProcess
+    {
+        public static void SaveCFileInfo2XML(CFileInfo cfi)
+        {
+            XDocument xdoc = new XDocument();
+        }
+    }
+}
