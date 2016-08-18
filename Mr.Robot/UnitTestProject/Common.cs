f67910a00eb5bf839124dfc4a436452fa8b3a4b7
gangjian <gangjian.mail@qq.com>
2016-08-18 17:34:33 +0800

    20160818

--- Mr.Robot/UnitTestProject/Common.cs
+++ Mr.Robot/UnitTestProject/Common.cs
@@ -40,5 +40,43 @@ namespace UnitTestProject
 			CCodeAnalyser.GetCodeBlockStructure(code_parse_result.SourceParseInfo.parsedCodeList, root_node);
 			return code_parse_result;
 		}
+
+		public static List<CCodeParseResult> UnitTest_SourceFileProcess2(string full_path)
+		{
+			if (string.IsNullOrEmpty(full_path))
+			{
+				return null;
+			}
+			int idx = full_path.LastIndexOf("\\");
+			if (-1 == idx)
+			{
+				return null;
+			}
+			string folder_path = full_path.Remove(idx);
+
+			List<string> source_list = new List<string>();
+			List<string> header_list = new List<string>();
+			// 取得所有源文件和头文件列表
+			IOProcess.GetAllCCodeFiles(folder_path, ref source_list, ref header_list);
+			// 解析指定的源文件,并取得解析结果
+			List<string> csfList = new List<string>();
+			csfList.Add(full_path);
+			List<CCodeParseResult> parseResultList = CCodeAnalyser.CFileListProcess(csfList, header_list);
+
+			return parseResultList;
+		}
+
+		public static CCodeParseResult UnitTest_GetFuncParseResult(string full_path, string fun_name, List<CCodeParseResult> parseResultList, ref StatementNode root_node)
+		{
+			CCodeParseResult code_parse_result;
+			CFunctionStructInfo funInfo = CCodeAnalyser.FindFileAndFunctionStructInfoFromParseResult(full_path, fun_name, parseResultList, out code_parse_result);
+
+			// 指定函数语句树结构的分析提取
+			root_node = new StatementNode();
+			root_node.Type = StatementNodeType.Root;
+			root_node.Scope = funInfo.Scope;
+			CCodeAnalyser.GetCodeBlockStructure(code_parse_result.SourceParseInfo.parsedCodeList, root_node);
+			return code_parse_result;
+		}
 	}
 }
---
+++ Mr.Robot/UnitTestProject/UnitTest_Rte_swc_in_oilp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mr.Robot;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class UnitTest_Rte_swc_in_oilp
+	{
+		static StatementNode root;                                                      // 函数语句结构的根节点
+		static List<CCodeParseResult> parseResultList;
+		static string source_name = "..\\..\\..\\TestSrc\\swc_in_oilp\\Rte_swc_in_oilp.c";
+
+		[ClassInitialize]
+		public static void TestClassSetup(TestContext ctx)
+		{
+			parseResultList = Common.UnitTest_SourceFileProcess2(source_name);
+		}
+
+		[TestMethod, TestCategory("Rte_swc_in_oilp.c")]
+		public void sym_rbl_in_oilp_IO()
+		{
+			CCodeParseResult c_source_file_parse_result = Common.UnitTest_GetFuncParseResult(source_name, "sym_rbl_in_oilp", parseResultList, ref root);
+
+			Assert.AreEqual(3, root.childList.Count);
+			Assert.AreEqual(StatementNodeType.Simple, root.childList[0].Type);
+			Assert.AreEqual(StatementNodeType.Simple, root.childList[1].Type);
+			Assert.AreEqual(StatementNodeType.Simple, root.childList[2].Type);
+
+			AnalysisContext analysisContext = CCodeAnalyser.FunctionStatementsAnalysis(root, c_source_file_parse_result);
+			Assert.AreEqual(3, analysisContext.calledFunctionList.Count);
+			Assert.AreEqual("makeEngOnOff3s()", analysisContext.calledFunctionList[0].Text);
+			Assert.AreEqual("makeIgv()", analysisContext.calledFunctionList[1].Text);
+			Assert.AreEqual("makeOilpAd()", analysisContext.calledFunctionList[2].Text);
+		}
+
+	}
+}
