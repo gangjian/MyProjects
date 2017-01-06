d7a07ab6de35270666bc04af857f071dea105551
gangjian <gangjian.mail@qq.com>
2017-01-06 17:59:12 +0800

    20170106

--- Mr.Robot/Mr.Robot/MacroSwitchAnalyser/MacroSwitchAnalyser.cs
+++ Mr.Robot/Mr.Robot/MacroSwitchAnalyser/MacroSwitchAnalyser.cs
@@ -42,6 +42,7 @@ namespace Mr.Robot.MacroSwitchAnalyser
 
 		void ProcessCodeLine(int line_num, string code_line, ref List<string> result_list)
 		{
+			code_line = RemoveStringSegment(code_line);
 			int idx = code_line.IndexOf("#if");
 			if (-1 == idx)
 			{
@@ -52,15 +53,46 @@ namespace Mr.Robot.MacroSwitchAnalyser
 			{
 				return;
 			}
-			List<StatementComponent> cpntList = StatementAnalysis.GetComponents(expStr, null);
-			foreach (StatementComponent cpnt in cpntList)
+			MacroPrintInfo printInfo = new MacroPrintInfo(this.SourceName, line_num.ToString(), code_line);
+			MacroSwitchExpressionAnalysis(expStr, printInfo, ref result_list);
+		}
+
+		string RemoveStringSegment(string code_line)
+		{
+			while (true)
 			{
-				if (cpnt.Type == StatementComponentType.Identifier)
+				int idx_s = code_line.IndexOf('\"');
+				if (   (-1 != idx_s)
+					&& (code_line.Length - 1 != idx_s) )
 				{
-					MacroPrintInfo printInfo = new MacroPrintInfo(this.SourceName, line_num.ToString(), code_line);
-					MacroAnalyzeProc(cpnt.Text, printInfo, ref result_list);
+					int idx_e = code_line.IndexOf('\"', idx_s + 1);
+					if (-1 != idx_e)
+					{
+						code_line = code_line.Remove(idx_s, idx_e - idx_s + 1);
+					}
+					else
+					{
+						break;
+					}
+				}
+				else
+				{
+					break;
 				}
 			}
+			return code_line;
+		}
+
+		string RemoveExpressionBrackets(string exp_str)
+		{
+			exp_str = exp_str.Trim();
+			if (exp_str.StartsWith("(")
+				&& exp_str.EndsWith(")"))
+			{
+				exp_str = exp_str.Remove(exp_str.Length - 1, 1);
+				exp_str = exp_str.Remove(0, 1).Trim();
+			}
+			return exp_str;
 		}
 
 		string GetMacroExpression(string code_line, int ifIdx)
@@ -75,29 +107,87 @@ namespace Mr.Robot.MacroSwitchAnalyser
 			return null;
 		}
 
-		void MacroAnalyzeProc(string macro_name, MacroPrintInfo print_info, ref List<string> result_list)
+		void MacroSwitchExpressionAnalysis(string macro_exp, MacroPrintInfo print_info, ref List<string> result_list)
 		{
-			MacroDefineInfo mdi = this.SourceParseInfo.FindMacroDefInfo(macro_name);
-			if (null != mdi)
+			if (string.IsNullOrEmpty(macro_exp))
 			{
-				string valStr = mdi.Value;
-				if (CommonProcess.IsStandardIdentifier(valStr))
+				return;
+			}
+			List<StatementComponent> cpntList = StatementAnalysis.GetComponents(macro_exp, null);
+			foreach (StatementComponent cpnt in cpntList)
+			{
+				if (cpnt.Type == StatementComponentType.Identifier && "defined" != cpnt.Text)
 				{
-					MacroAnalyzeProc(valStr, print_info, ref result_list);
+					MacroDefineInfo mdi = this.SourceParseInfo.FindMacroDefInfo(cpnt.Text);
+					if (null != mdi)
+					{
+						string valStr = mdi.Value;
+						// 宏值分析
+						MacroValueType mType = MacroValueStrAnalysis(ref valStr);
+						// 立即数
+						if (mType == MacroValueType.ConstNumber)
+						{
+							string resultStr = MakeResultStr(mdi.Name, valStr, print_info);
+							result_list.Add(resultStr);
+						}
+						// 表达式
+						else if (mType == MacroValueType.Expression)
+						{
+							MacroSwitchExpressionAnalysis(valStr, print_info, ref result_list);
+						}
+						// 空
+						else if (mType == MacroValueType.Empty)
+						{
+							string resultStr = MakeResultStr(mdi.Name, string.Empty, print_info);
+							result_list.Add(resultStr);
+						}
+					}
+					else
+					{
+						// 未定义
+						string resultStr = MakeResultStr(cpnt.Text, @"未定义", print_info);
+						result_list.Add(resultStr);
+					}
 				}
-				else
+			}
+		}
+
+		MacroValueType MacroValueStrAnalysis(ref string macro_value_str)
+		{
+			if (string.IsNullOrEmpty(macro_value_str.Trim()))
+			{
+				return MacroValueType.Empty;
+			}
+			macro_value_str = RemoveExpressionBrackets(macro_value_str);
+			List<StatementComponent> cpntList = StatementAnalysis.GetComponents(macro_value_str, null);
+			int numberCount = 0;
+			int identifierCount = 0;
+			foreach (StatementComponent cpnt in cpntList)
+			{
+				if (cpnt.Type == StatementComponentType.ConstantNumber)
 				{
-					string resultStr = print_info.SourceName + "," + print_info.LineNumStr
-							+ "," + print_info.CodeText + "," + macro_name + "," + valStr;
-					result_list.Add(resultStr);
+					numberCount += 1;
 				}
+				else if (cpnt.Type == StatementComponentType.Identifier && cpnt.Text != "defined")
+				{
+					identifierCount += 1;
+				}
+			}
+			if (1 == numberCount && 0 == identifierCount)
+			{
+				return MacroValueType.ConstNumber;
 			}
 			else
 			{
-				string resultStr = print_info.SourceName + "," + print_info.LineNumStr
-							+ "," + print_info.CodeText + "," + macro_name + "," + @"未定义";
-				result_list.Add(resultStr);
+				return MacroValueType.Expression;
 			}
+		}
+
+		string MakeResultStr(string macro_name, string value_str, MacroPrintInfo print_info)
+		{
+			string resultStr = print_info.SourceName + "," + print_info.LineNumStr
+							+ "," + print_info.CodeText + "," + macro_name + "," + value_str;
+			return resultStr;
 		}
 
 		class MacroPrintInfo
@@ -112,6 +202,13 @@ namespace Mr.Robot.MacroSwitchAnalyser
 				this.LineNumStr = line_num;
 				this.CodeText = code_text.Trim().Replace('\t', ' ');
 			}
+		}
+
+		enum MacroValueType
+		{
+			ConstNumber,
+			Expression,
+			Empty,
 		}
 	}
 }
