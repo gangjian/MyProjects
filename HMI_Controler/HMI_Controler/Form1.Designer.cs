7a5a575c7be8dd4e934689804bca15c7b0e81cc1
gangjian <gangjian.mail@qq.com>
2017-02-27 08:28:19 +0800

    20170226

---
+++ HMI_Controler/HMI_Controler/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace HMI_Controler
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.button5 = new System.Windows.Forms.Button();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.button6 = new System.Windows.Forms.Button();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.button7 = new System.Windows.Forms.Button();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.button8 = new System.Windows.Forms.Button();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.tbxExePath = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(46, 50);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(103, 31);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(172, 56);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(748, 21);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "change_page;2";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(46, 87);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(103, 31);
+			this.button2.TabIndex = 0;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(172, 93);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(748, 21);
+			this.textBox2.TabIndex = 1;
+			this.textBox2.Text = "set_ctrl_property;ITEM_TYPE=TEXT_PROGRESSBAR;ID=2002;VALUE=140;TOTAL=360;POS_X=70" +
+    ";POS_Y=300;WIDTH=660;HEIGHT=20";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(46, 124);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(103, 31);
+			this.button3.TabIndex = 0;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(172, 130);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(748, 21);
+			this.textBox3.TabIndex = 1;
+			this.textBox3.Text = "set_ctrl_property;ITEM_TYPE=TEXT_BOX;ID=2001;FIXED_TEXT=残り時間;TEXT=25:17;POS_X=260" +
+    ";POS_Y=200;WIDTH=240;HEIGHT=40";
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(46, 161);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(103, 31);
+			this.button4.TabIndex = 0;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(172, 167);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(748, 21);
+			this.textBox4.TabIndex = 1;
+			this.textBox4.Text = "set_ctrl_property;ITEM_TYPE=BUTTON;ID=2003;DISPLAYED=TRUE;SELECTED=FALSE;ENABLED=" +
+    "TRUE;FIXED_TEXT=キャンセル;POS_X=300;POS_Y=400;WIDTH=150;HEIGHT=35";
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(46, 198);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(103, 31);
+			this.button5.TabIndex = 0;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(172, 204);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(748, 21);
+			this.textBox5.TabIndex = 1;
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(46, 235);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(103, 31);
+			this.button6.TabIndex = 0;
+			this.button6.Text = "button6";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(172, 241);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(748, 21);
+			this.textBox6.TabIndex = 1;
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(46, 272);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(103, 31);
+			this.button7.TabIndex = 0;
+			this.button7.Text = "button7";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(172, 278);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(748, 21);
+			this.textBox7.TabIndex = 1;
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(46, 309);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(103, 31);
+			this.button8.TabIndex = 0;
+			this.button8.Text = "button8";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(172, 315);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(748, 21);
+			this.textBox8.TabIndex = 1;
+			// 
+			// tbxExePath
+			// 
+			this.tbxExePath.Location = new System.Drawing.Point(49, 12);
+			this.tbxExePath.Name = "tbxExePath";
+			this.tbxExePath.Size = new System.Drawing.Size(871, 21);
+			this.tbxExePath.TabIndex = 2;
+			this.tbxExePath.Text = "C:\\Users\\GangJian\\03_work\\github\\MyProjects\\HMI_Client_Demo\\Debug\\HMI_Client_Demo" +
+    ".exe";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(938, 360);
+			this.Controls.Add(this.tbxExePath);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.TextBox tbxExePath;
+	}
+}
+
---
+++ HMI_Controler/HMI_Controler/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace HMI_Controler
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			this.ExePath = tbxExePath.Text;
+		}
+
+		string ExePath = "C:\\Users\\GangJian\\03_work\\github\\MyProjects\\HMI_Client_Demo\\Debug\\HMI_Client_Demo.exe";
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			SendCmdStr(textBox1.Text);
+		}
+
+		void SendCmdStr(string cmd_str)
+		{
+			if (string.IsNullOrEmpty(cmd_str))
+			{
+				return;
+			}
+			Process clientProcess = new Process();
+			clientProcess.StartInfo = new ProcessStartInfo(this.ExePath, cmd_str);
+			clientProcess.StartInfo.UseShellExecute = false;
+			clientProcess.StartInfo.CreateNoWindow = true;
+			clientProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			clientProcess.Start();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			SendCmdStr(textBox2.Text);
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			SendCmdStr(textBox3.Text);
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			SendCmdStr(textBox4.Text);
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			SendCmdStr(textBox5.Text);
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			SendCmdStr(textBox6.Text);
+		}
+
+		private void button7_Click(object sender, EventArgs e)
+		{
+			SendCmdStr(textBox7.Text);
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+			SendCmdStr(textBox8.Text);
+		}
+	}
+}
