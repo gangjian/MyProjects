5c51ae4f164e08a2adfa0d9dd551fbcccf4cb1e2
gangjian <gangjian.mail@qq.com>
2017-10-28 08:08:33 +0800

    20171028

---
+++ M4A_MP3_Collector/M4A_MP3_Collector/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace M4A_MP3_Collector
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tbxA = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tbxB = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnCopyM4A = new System.Windows.Forms.Button();
+			this.btnReplaceM4A = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// tbxA
+			// 
+			this.tbxA.Location = new System.Drawing.Point(12, 33);
+			this.tbxA.Name = "tbxA";
+			this.tbxA.ReadOnly = true;
+			this.tbxA.Size = new System.Drawing.Size(425, 22);
+			this.tbxA.TabIndex = 0;
+			this.tbxA.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(447, 32);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "...";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// tbxB
+			// 
+			this.tbxB.Location = new System.Drawing.Point(12, 97);
+			this.tbxB.Name = "tbxB";
+			this.tbxB.ReadOnly = true;
+			this.tbxB.Size = new System.Drawing.Size(425, 22);
+			this.tbxB.TabIndex = 0;
+			this.tbxB.TabStop = false;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(447, 96);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "...";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(63, 14);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Folder A";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 80);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(63, 14);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Folder B";
+			// 
+			// btnCopyM4A
+			// 
+			this.btnCopyM4A.Location = new System.Drawing.Point(12, 168);
+			this.btnCopyM4A.Name = "btnCopyM4A";
+			this.btnCopyM4A.Size = new System.Drawing.Size(150, 42);
+			this.btnCopyM4A.TabIndex = 3;
+			this.btnCopyM4A.Text = "Copy M4A in A to B";
+			this.btnCopyM4A.UseVisualStyleBackColor = true;
+			this.btnCopyM4A.Click += new System.EventHandler(this.btnCopyM4A_Click);
+			// 
+			// btnReplaceM4A
+			// 
+			this.btnReplaceM4A.Location = new System.Drawing.Point(287, 168);
+			this.btnReplaceM4A.Name = "btnReplaceM4A";
+			this.btnReplaceM4A.Size = new System.Drawing.Size(150, 42);
+			this.btnReplaceM4A.TabIndex = 3;
+			this.btnReplaceM4A.Text = "Replace M4A in A By MP3 in B";
+			this.btnReplaceM4A.UseVisualStyleBackColor = true;
+			this.btnReplaceM4A.Click += new System.EventHandler(this.btnReplaceM4A_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(534, 240);
+			this.Controls.Add(this.btnReplaceM4A);
+			this.Controls.Add(this.btnCopyM4A);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.tbxB);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.tbxA);
+			this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "M4A_MP3_Collector";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox tbxA;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox tbxB;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnCopyM4A;
+		private System.Windows.Forms.Button btnReplaceM4A;
+	}
+}
+
---
+++ M4A_MP3_Collector/M4A_MP3_Collector/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace M4A_MP3_Collector
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fbd = new FolderBrowserDialog();
+			if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				this.tbxA.Text = fbd.SelectedPath;
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fbd = new FolderBrowserDialog();
+			if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				this.tbxB.Text = fbd.SelectedPath;
+			}
+		}
+
+		bool CheckStart()
+		{
+			if (!string.IsNullOrEmpty(tbxA.Text)
+				&& !string.IsNullOrEmpty(tbxB.Text)
+				&& Directory.Exists(tbxA.Text)
+				&& Directory.Exists(tbxB.Text))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		private void btnCopyM4A_Click(object sender, EventArgs e)
+		{
+			if (!CheckStart())
+			{
+				return;
+			}
+			string destDir = tbxB.Text + "\\";
+			try
+			{
+				int counter = 0;
+				FileInfo[] M4aFiles = GetAllFiles(tbxA.Text, "*.m4a");
+				foreach (var item in M4aFiles)
+				{
+					File.Copy(item.FullName, destDir + item.Name, true);
+					counter += 1;
+				}
+				MessageBox.Show(counter.ToString() + " M4A files Copied!");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void btnReplaceM4A_Click(object sender, EventArgs e)
+		{
+			if (!CheckStart())
+			{
+				return;
+			}
+			try
+			{
+				FileInfo[] M4aFiles = GetAllFiles(tbxA.Text, "*.m4a");						// 从A下取得所有M4A文件
+				FileInfo[] Mp3Files = GetAllFiles(tbxB.Text, "*.mp3");						// 从B下取得所有MP3文件
+				List<string> Mp3NameList = new List<string>();
+				foreach (var item in Mp3Files)
+				{
+					string nameWithoutExt = item.Name.Remove(item.Name.Length - item.Extension.Length);
+					Mp3NameList.Add(nameWithoutExt);
+				}
+				int counter = 0;
+				foreach (var item in M4aFiles)
+				{
+					// 如果在B下有同名的MP3文件
+					string nameWithoutExt = item.Name.Remove(item.Name.Length - item.Extension.Length);
+					if (Mp3NameList.Contains(nameWithoutExt))
+					{
+						// 把同名的Mp3文件拷到M4a文件的路径下
+						string srcFile = this.tbxB.Text + "\\" + nameWithoutExt + ".mp3";
+						File.Copy(srcFile, item.DirectoryName + "\\" + nameWithoutExt + ".mp3", true);
+						// 删除原来的M4a文件
+						File.Delete(item.FullName);
+						counter += 1;
+					}
+				}
+				MessageBox.Show("Success to Replace " + counter.ToString() + " M4A Files!");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		FileInfo[] GetAllFiles(string path, string search_pattern)
+		{
+			System.Diagnostics.Trace.Assert(!string.IsNullOrEmpty(path)
+											&& !string.IsNullOrEmpty(search_pattern)
+											&& Directory.Exists(path));
+			DirectoryInfo di = new DirectoryInfo(path);
+			FileInfo[] fileArr = di.GetFiles(search_pattern, SearchOption.AllDirectories);
+			return fileArr;
+		}
+	}
+}
