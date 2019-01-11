578a7082a23373f1bd4f7c3e382dce1c4911a3a2
gangjian <gangjian.mail@qq.com>
2019-01-11 15:24:57 +0800

    20190111

--- CodeCreeper/CodeCreeper/Info/IncludeInfo.cs
+++ CodeCreeper/CodeCreeper/Info/IncludeInfo.cs
@@ -19,9 +19,16 @@ namespace CodeCreeper
 
 		IncludeInfo(CodeElement inc_element, CodePosition left_quote_pos,
 					CodePosition right_quote_pos, CodePosition header_pos,
-					string header_name, string path)
+					string header_name, string path,
+					QuoteBracketType q_type = QuoteBracketType.Quote)
 		{
-
+			this.IncludeElement = inc_element;
+			this.LeftQuotePos = left_quote_pos;
+			this.RightQuotePos = right_quote_pos;
+			this.HeaderPos = header_pos;
+			this.HeaderName = header_name;
+			this.FilePath = path;
+			this.QuoteType = q_type;
 		}
 
 		public static IncludeInfo Parse(CodeElement inc_element, CodeFileInfo file_info)
@@ -39,17 +46,30 @@ namespace CodeCreeper
 				string header_name = header_str.Substring(1, header_str.Length - 2).Trim();
 				int idx = header_str.IndexOf(header_name);
 				Trace.Assert(-1 != idx);
-				CodePosition header_pos
-					= new CodePosition(element_list[1].GetStartPosition().Row,
-										element_list[1].GetStartPosition().Col + idx);
-				ret_info = new IncludeInfo(	inc_element,
-										element_list[1].GetStartPosition(),
-										element_list[1].EndPos, header_pos, header_name,
-										file_info.FullName);
+				CodePosition lp = element_list[1].GetStartPosition();
+				CodePosition rp = element_list[1].EndPos;
+				CodePosition header_pos	= new CodePosition(	lp.Row, lp.Col + idx);
+				ret_info = new IncludeInfo(inc_element, lp, rp, header_pos, header_name,
+											file_info.FullName);
 			}
 			else
 			{
-				Trace.Assert(false);
+				Trace.Assert(6 == element_list.Count);
+				Trace.Assert(element_list[1].ToString(file_info.CodeList).Equals("<"));
+				Trace.Assert(element_list[5].ToString(file_info.CodeList).Equals(">"));
+				Trace.Assert(element_list[3].ToString(file_info.CodeList).Equals("."));
+				Trace.Assert(element_list[2].Type == ElementType.Identifier);
+				Trace.Assert(element_list[4].Type == ElementType.Identifier);
+				Trace.Assert(element_list[2].CloseTo(element_list[3], file_info.CodeList));
+				Trace.Assert(element_list[3].CloseTo(element_list[4], file_info.CodeList));
+				CodePosition lp = element_list[1].GetStartPosition();
+				CodePosition rp = element_list[5].GetStartPosition();
+				CodePosition hp = element_list[2].GetStartPosition();
+				string hname = element_list[2].ToString(file_info.CodeList)
+								+ element_list[3].ToString(file_info.CodeList)
+								+ element_list[4].ToString(file_info.CodeList);
+				ret_info = new IncludeInfo(	inc_element, lp, rp, hp, hname,
+											file_info.FullName, QuoteBracketType.Angle);
 			}
 			return ret_info;
 		}
